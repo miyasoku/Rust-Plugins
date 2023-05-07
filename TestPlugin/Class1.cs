64521806efeab97dc178bc4df637ac4522b8067b
81704 <81704@DESKTOP-7PITCHV>
2023-05-07 16:37:04 +0900

    プロジェクト ファイルを追加します。

---
+++ TestPlugin/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Plugins
+{
+    [Info("Test Plugin", "miyasoku", 1.0)]
+    [Description("The simple oxide plugins.")]
+
+    public class Main : RustPlugin
+    {
+        [ChatCommand("hello")]
+        void HelloCommand(BasePlayer player)
+        {
+            SendMessage(player, "Hello world!" + player.displayName);
+        }
+        void SendMessage(BasePlayer player, string msg, params object[] args)
+        {
+            PrintToChat(player, msg, args);
+        }
+        void Broadcast(string msg, params object[] args)
+        {
+            PrintToChat(msg);
+        }
+        void Loaded()
+        {
+            Broadcast("The test plugins has been reloaded!");
+        }
+    }
+}
